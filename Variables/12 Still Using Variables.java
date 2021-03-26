5d67cbf15d538a244aa08ce6975a8c9bd983dd16
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-26 15:17:59 +0530

    Create 12 Still Using Variables.java

---
+++ Variables/12 Still Using Variables.java
@@ -0,0 +1,11 @@
+public class StillUsingVariables {
+    public static void main (String[] args) {
+        int myGraduationYear;
+        String myName;
+
+        myGraduationYear = 2015;
+        myName = "Ashish Shejale";
+
+        System.out.println("My name is " + myName + " and I'll graduate in " + myGraduationYear + ".");
+    }
+}
