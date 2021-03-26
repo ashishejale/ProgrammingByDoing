50b8658fbb17047471b0118d43b1b1f66046202c
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-26 15:11:41 +0530

    Create 11 Using Variables.java

---
+++ Variables/11 Using Variables.java
@@ -0,0 +1,17 @@
+
+public class UsingVariables {
+    public static void main (String[] args) {
+        
+      int roomNumber;
+      double e;
+      String subject;
+
+      roomNumber = 113;
+      e = 2.71828;
+      subject = "Computer Science";
+
+      System.out.println("This room in # " + roomNumber);
+      System.out.println("e is close to " + e);
+      System.out.println("I am learning a bit about " + subject);
+    }
+}
