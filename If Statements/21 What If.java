584fd8bb2898bbe16fd48ad459f7712541f3e6b9
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-29 17:33:56 +0530

    Create 21 What If.java

---
+++ If Statements/21 What If.java
@@ -0,0 +1,38 @@
+public class WhatIf {
+	public static void main( String[] args ) {
+
+      int people = 30; //when people == cats no message of cats is printed
+      int cats = 30;
+      int dogs = 15;
+
+      if ( people < cats ) { //if people is < cats prints below message else exits if statement
+          System.out.println( "Too many cats!  The world is doomed!" );
+      }
+
+      if ( people > cats ) { //if people is > cats prints below message else exits if statement
+          System.out.println( "Not many cats!  The world is saved!" );
+      }
+
+      if ( people < dogs ) {  //if people is < dogs prints below message else exits if statement
+          System.out.println( "The world is drooled on!" );
+      }
+
+      if ( people > dogs ) { //if people is > dogs prints below message else exits if statement
+          System.out.println( "The world is dry!" );
+      }
+
+      dogs += 5;
+
+      if ( people >= dogs ) { //if people is > or = dogs prints below message else exits if statement
+          System.out.println( "People are greater than or equal to dogs." );
+      }
+
+      if ( people <= dogs ) { //if people is < or = dogs prints below message else exits if statement
+          System.out.println( "People are less than or equal to dogs." );
+      }
+
+      if ( people == dogs ) { //if people is exactly = dogs prints below message else exits if statement
+          System.out.println( "People are dogs." );
+      }
+  }
+}
